8a78bd835af118e7b49132c52dbe417567f557b2
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-01 23:49:04 -0700

    Add initial LibGDX template project.

---
+++ desktop/src/com/danilafe/duality/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.danilafe.duality.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.danilafe.duality.Duality;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Duality(), config);
+	}
+}
