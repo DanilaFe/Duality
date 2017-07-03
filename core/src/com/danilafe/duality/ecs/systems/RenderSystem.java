796d4ea9cb8cbc5bec91fbe609f808b66e75028e
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-03 12:42:21 -0700

    Implement the desired aesthetic.

--- core/src/com/danilafe/duality/ecs/systems/RenderSystem.java
+++ core/src/com/danilafe/duality/ecs/systems/RenderSystem.java
@@ -6,8 +6,11 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.danilafe.duality.ecs.components.Animated;
 import com.danilafe.duality.ecs.components.Position;
@@ -15,6 +18,7 @@ import com.danilafe.duality.ecs.components.Position;
 public class RenderSystem extends IteratingSystem {
 
     public OrthographicCamera activeCamera;
+    public Texture backgroundTexture;
 
     private SpriteBatch mainBatch;
     private SpriteBatch textureBatch;
@@ -24,8 +28,14 @@ public class RenderSystem extends IteratingSystem {
 
     public RenderSystem(){
         super(Family.all(Animated.class, Position.class).get());
+        ShaderProgram.pedantic = false;
+        shaderProgram = new ShaderProgram(Gdx.files.internal("vertex.glsl").readString(), Gdx.files.internal("fragment.glsl").readString());
+        if(!shaderProgram.isCompiled()){
+            System.err.println(shaderProgram.getLog());
+        }
 
         mainBatch = new SpriteBatch();
+        mainBatch.setShader(shaderProgram);
 
         textureBatch = new SpriteBatch();
 
@@ -34,6 +44,7 @@ public class RenderSystem extends IteratingSystem {
 
     private void activate(SpriteBatch batch){
         activeBatch = batch;
+        activeBatch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         activeBatch.begin();
     }
 
@@ -48,7 +59,7 @@ public class RenderSystem extends IteratingSystem {
         Position pos = entity.getComponent(Position.class);
 
         anm.currentDelay -= deltaTime;
-        if(anm.currentDelay <= 0){
+        while(anm.currentDelay <= 0 && anm.frameDelay != 0){
             anm.currentDelay += anm.frameDelay;
             anm.currentFrame++;
             if(anm.currentFrame >= anm.currentFrames.size()){
@@ -63,14 +74,39 @@ public class RenderSystem extends IteratingSystem {
 
     @Override
     public void update(float deltaTime) {
-        if(activeCamera != null){
-            mainBatch.setProjectionMatrix(activeCamera.combined);
+        FrameBuffer gameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
+        gameBuffer.begin();
+        activate(textureBatch);
+        {
+            Gdx.gl.glClearColor(0,0, 0, 0);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            if(activeCamera != null)
+                activeBatch.setProjectionMatrix(activeCamera.combined);
+            super.update(deltaTime);
         }
+        deactivate();
+        gameBuffer.end();
+
+        FrameBuffer backgroundBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
+        backgroundBuffer.begin();
+        activate(textureBatch);
+        {
+            Gdx.gl.glClearColor(0, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            if(backgroundTexture != null)
+                textureBatch.draw(backgroundTexture, 0, 0);
+        }
+        deactivate();
+        backgroundBuffer.end();
+
         activate(mainBatch);
         {
+            backgroundBuffer.getColorBufferTexture().bind(1);
             Gdx.gl.glClearColor(1, 1, 1, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            super.update(deltaTime);
+            Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
+            activeBatch.getShader().setUniformi("u_background", 1);
+            activeBatch.draw(gameBuffer.getColorBufferTexture(), 0, Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), -Gdx.graphics.getHeight());
         }
         deactivate();
     }
