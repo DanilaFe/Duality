208c44e0c64b61455b3a7e73cc322cbd0bb34628
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-05 14:37:09 -0700

    Add transitions between players using their IDs.

--- core/src/com/danilafe/duality/ecs/systems/PlayerSystem.java
+++ core/src/com/danilafe/duality/ecs/systems/PlayerSystem.java
@@ -1,9 +1,12 @@
 package com.danilafe.duality.ecs.systems;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.IntIntMap;
+import com.badlogic.gdx.utils.IntMap;
 import com.danilafe.duality.ecs.components.*;
 
 public class PlayerSystem extends IteratingSystem {
@@ -15,8 +18,30 @@ public class PlayerSystem extends IteratingSystem {
     static final float PLAYER_FRICTION_STANDING = 1;
     static final float PLAYER_FRICTION_MOVING = .25f;
 
+    public IntIntMap keysToId;
+    public IntMap<Boolean> idTransitions;
+
     public PlayerSystem(){
         super(Family.all(Player.class, Acceleration.class, Velocity.class, FrictionEntity.class, Animated.class, SurfaceTracker.class).get());
+        keysToId = new IntIntMap();
+        idTransitions = new IntMap<>();
+    }
+
+    public void registerPlayer(int id, int key, boolean transiton){
+        keysToId.put(key, id);
+        idTransitions.put(id, transiton);
+    }
+    public void switchPlayer(int id){
+        Entity newPlayer = null;
+        for(Entity playerEntity : getEntities()){
+            Player player = playerEntity.getComponent(Player.class);
+            player.active = player.switchId == id;
+            if(player.active) newPlayer = playerEntity;
+        }
+        Engine engine = getEngine();
+        engine.getEntitiesFor(Family.all(Following.class, Camera.class).get())
+                .first().getComponent(Following.class).otherEntity = newPlayer;
+        engine.getSystem(RenderSystem.class).increasing = idTransitions.get(id);
     }
 
     @Override
@@ -46,4 +71,12 @@ public class PlayerSystem extends IteratingSystem {
         if(Gdx.input.isKeyJustPressed(player.jumpKey) && entity.getComponent(SurfaceTracker.class).onSurface) velocity.velocity.y = PLAYER_VELOCITY_JUMP;
     }
 
+    @Override
+    public void update(float deltaTime) {
+        for(IntIntMap.Entry entry : keysToId.entries()){
+            if(Gdx.input.isKeyPressed(entry.key)) switchPlayer(entry.value);
+        }
+
+        super.update(deltaTime);
+    }
 }
