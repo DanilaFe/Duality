d601b76c94b984110a3d909599be2f217e10d17e
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-05 20:09:33 -0700

    Add an "Average" component that puts the entity between given entities

---
+++ core/src/com/danilafe/duality/ecs/systems/AverageSystem.java
@@ -0,0 +1,32 @@
+package com.danilafe.duality.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
+import com.danilafe.duality.ecs.components.Average;
+import com.danilafe.duality.ecs.components.Position;
+import javafx.geometry.Pos;
+
+public class AverageSystem extends IteratingSystem {
+
+    public AverageSystem(){
+        super(Family.all(Average.class, Position.class).get());
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        Position position = entity.getComponent(Position.class);
+        Average average = entity.getComponent(Average.class);
+
+        if(average.entities.size != 0){
+            position.position.set(0, 0);
+            for(Entity following : average.entities){
+                Position followingPos = following.getComponent(Position.class);
+                position.position.add(followingPos.position);
+            }
+            position.position.scl(1.f / average.entities.size);
+        }
+    }
+
+}
