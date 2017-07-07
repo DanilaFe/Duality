0346d39af67c144fd06e1ceea94f6525a9703113
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-06 20:53:15 -0700

    Add an "expiring" component.

---
+++ core/src/com/danilafe/duality/ecs/systems/ExpiringSystem.java
@@ -0,0 +1,20 @@
+package com.danilafe.duality.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.danilafe.duality.ecs.EntityUtils;
+import com.danilafe.duality.ecs.components.Expiring;
+
+public class ExpiringSystem extends IteratingSystem {
+
+    public ExpiringSystem() {
+        super(Family.all(Expiring.class).get());
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        if((entity.getComponent(Expiring.class).expireTime -= deltaTime) < 0)
+            EntityUtils.safeDelete(getEngine(), entity);
+    }
+}
