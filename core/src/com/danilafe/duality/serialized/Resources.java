3e550afe7c91719879b107e5089e20396db41167
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-06 18:39:25 -0700

    Add default variable initializations.

--- core/src/com/danilafe/duality/serialized/Resources.java
+++ core/src/com/danilafe/duality/serialized/Resources.java
@@ -6,13 +6,13 @@ public class Resources {
 
     public static class Animation {
         public String rawTexture;
-        public ObjectMap<String, Integer[]> animations;
-        public int[] dimensions;
+        public ObjectMap<String, Integer[]> animations = new ObjectMap<>();
+        public int[] dimensions = new int[2];
     }
 
-    public ObjectMap<String, Animation> animations;
-    public ObjectMap<String, String> textures;
-    public String[] shaders;
-    public String[] rawTextures;
+    public ObjectMap<String, Animation> animations = new ObjectMap<>();
+    public ObjectMap<String, String> textures = new ObjectMap<>();
+    public String[] shaders = new String[0];
+    public String[] rawTextures = new String[0];
 
 }
