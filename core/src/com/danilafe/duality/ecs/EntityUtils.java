d9abb3c3910a3b502d5f01fd24a36fcfa996109f
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-06 19:34:29 -0700

    Added a utility function to create a decorative entity. A decorative entity is just a sprite and a position.

---
+++ core/src/com/danilafe/duality/ecs/EntityUtils.java
@@ -0,0 +1,31 @@
+package com.danilafe.duality.ecs;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.danilafe.duality.ResourceManager;
+import com.danilafe.duality.ecs.components.Animated;
+import com.danilafe.duality.ecs.components.Average;
+import com.danilafe.duality.ecs.components.Position;
+import javafx.geometry.Pos;
+
+public class EntityUtils {
+
+    public static Entity createDecorativeEntity(PooledEngine engine, ResourceManager resourceManager, RecipeDatabase database,
+                                         String resourceName, String animationName,
+                                         boolean loop, float x, float y){
+        Entity entity = engine.createEntity();
+        Animated animated = engine.createComponent(Animated.class);
+        animated.animationData = resourceManager.getAnimation(resourceName);
+        animated.play(animationName, loop);
+        TextureRegion frame = animated.animationData.getFrame(0);
+        Position position = engine.createComponent(Position.class);
+        position.position.set(x, y + frame.getRegionHeight() / 2);
+        entity.add(animated);
+        entity.add(position);
+        return entity;
+    }
+
+}
