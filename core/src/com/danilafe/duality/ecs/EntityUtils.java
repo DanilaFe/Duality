93e6b0d12a69e88aca21cab8f259a16d7e692ec8
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-06 20:51:35 -0700

    Add a safe delete function to prevent invalid entity access.

--- core/src/com/danilafe/duality/ecs/EntityUtils.java
+++ core/src/com/danilafe/duality/ecs/EntityUtils.java
@@ -1,14 +1,13 @@
 package com.danilafe.duality.ecs;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.danilafe.duality.ResourceManager;
-import com.danilafe.duality.ecs.components.Animated;
-import com.danilafe.duality.ecs.components.Average;
-import com.danilafe.duality.ecs.components.Position;
+import com.danilafe.duality.ecs.components.*;
 import javafx.geometry.Pos;
 
 public class EntityUtils {
@@ -26,6 +25,30 @@ public class EntityUtils {
         entity.add(animated);
         entity.add(position);
         return entity;
+    }
+
+    public static void safeDelete(Engine engine, Entity toDelete){
+        engine.getEntitiesFor(Family.all(Average.class).get()).forEach(e -> {
+            Average average = e.getComponent(Average.class);
+            average.entities.removeValue(toDelete, false);
+        });
+        engine.getEntitiesFor(Family.all(Child.class).get()).forEach(e -> {
+            Child child = e.getComponent(Child.class);
+            if(child.parent == toDelete) child.parent = null;
+        });
+        engine.getEntitiesFor(Family.all(Following.class).get()).forEach(e -> {
+           Following following = e.getComponent(Following.class);
+           if(following.otherEntity == toDelete) following.otherEntity = null;
+        });
+        engine.getEntitiesFor(Family.all(Parent.class).get()).forEach(e -> {
+            Parent parent = e.getComponent(Parent.class);
+            parent.children.removeValue(toDelete, false);
+        });
+        engine.getEntitiesFor(Family.all(SurfaceTracker.class).get()).forEach(e -> {
+            SurfaceTracker tracker = e.getComponent(SurfaceTracker.class);
+            if(tracker.lastSurface == toDelete) tracker.lastSurface = null;
+        });
+        engine.removeEntity(toDelete);
     }
 
 }
