ff03bc1827ad6bbab235fa50ef813bbcc8298ce4
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-07 12:59:22 -0700

    Add overlap tracking components.

---
+++ core/src/com/danilafe/duality/ecs/systems/OverlapSystem.java
@@ -0,0 +1,32 @@
+package com.danilafe.duality.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.danilafe.duality.ecs.components.CollisionBox;
+import com.danilafe.duality.ecs.components.OverlapTracker;
+import com.danilafe.duality.ecs.components.Overlapping;
+import com.danilafe.duality.ecs.systems.util.DualSystem;
+
+public class OverlapSystem extends DualSystem {
+
+    public OverlapSystem() {
+        super(Family.all(Overlapping.class, CollisionBox.class).get(),
+                Family.all(OverlapTracker.class, CollisionBox.class).get());
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        for(Entity tracker : entitiesB){
+            OverlapTracker trackerComponent = tracker.getComponent(OverlapTracker.class);
+            CollisionBox collisionBox = tracker.getComponent(CollisionBox.class);
+
+            trackerComponent.entities.clear();
+            for(Entity overlapCheck : entitiesA){
+                CollisionBox otherCollisionBox = overlapCheck.getComponent(CollisionBox.class);
+                if(collisionBox.box.overlaps(otherCollisionBox.box))
+                    trackerComponent.entities.add(overlapCheck);
+            }
+        }
+    }
+    
+}
