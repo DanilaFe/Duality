3b1b21c9b6781b43295531287ae9c0a05c08a294
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-07 20:40:36 -0700

    Add a LevelPortal component and a LevelSystem.

---
+++ core/src/com/danilafe/duality/ecs/components/LevelPortal.java
@@ -0,0 +1,18 @@
+package com.danilafe.duality.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+import com.danilafe.duality.level.Level;
+
+import java.util.function.Supplier;
+
+public class LevelPortal implements Component, Pool.Poolable {
+
+    public Supplier<Level> levelSupplier;
+
+    @Override
+    public void reset() {
+        levelSupplier = null;
+    }
+
+}
---
+++ core/src/com/danilafe/duality/ecs/systems/LevelSystem.java
@@ -0,0 +1,48 @@
+package com.danilafe.duality.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.danilafe.duality.ResourceManager;
+import com.danilafe.duality.ecs.EntityUtils;
+import com.danilafe.duality.ecs.RecipeDatabase;
+import com.danilafe.duality.ecs.components.ActiveGroup;
+import com.danilafe.duality.ecs.components.LevelPortal;
+import com.danilafe.duality.ecs.components.OverlapTracker;
+import com.danilafe.duality.ecs.components.Player;
+import com.danilafe.duality.ecs.systems.util.DualSystem;
+import com.danilafe.duality.level.Level;
+
+public class LevelSystem extends DualSystem {
+
+    public int levelTransitKey = Input.Keys.UP;
+    public Level currentLevel;
+    public Level toLoad;
+
+    public LevelSystem() {
+        super(Family.all(Player.class, ActiveGroup.class).get(),
+                Family.all(OverlapTracker.class, LevelPortal.class).get());
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        toLoad = null;
+        for(Entity portalEntity : entitiesB){
+            LevelPortal portal = portalEntity.getComponent(LevelPortal.class);
+            OverlapTracker tracker = portalEntity.getComponent(OverlapTracker.class);
+
+            boolean canTransition = true;
+            for(Entity otherEntity : entitiesA){
+                if(otherEntity.getComponent(ActiveGroup.class).active)
+                    canTransition &= tracker.entities.contains(otherEntity, false);
+            }
+
+            if(canTransition && Gdx.input.isKeyJustPressed(levelTransitKey) && portal.levelSupplier != null){
+                toLoad = portal.levelSupplier.get();
+                return;
+            }
+        }
+    }
+}
