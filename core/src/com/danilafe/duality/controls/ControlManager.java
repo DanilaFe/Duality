fac332750ee1b071de31d33a0eaef1cf4a8f8c43
Danila Fedorin <danila.fedorin@gmail.com>
2017-07-09 16:41:47 -0700

    Remove unused code.

--- core/src/com/danilafe/duality/controls/ControlManager.java
+++ core/src/com/danilafe/duality/controls/ControlManager.java
@@ -13,20 +13,12 @@ public class ControlManager {
 
     public ControlManager() {
         loadControls();
-        loadGroupKeys();
     }
 
     private void loadControls() {
         controls = new Array<>();
         controls.add(new ControlData(Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.UP, Input.Keys.DOWN, Input.Keys.SLASH, Input.Keys.SHIFT_RIGHT, null, 0, 0, 1, 2, 3));
         controls.add(new ControlData(Input.Keys.A, Input.Keys.D, Input.Keys.W, Input.Keys.S, Input.Keys.E, Input.Keys.Q, null, 0, 0, 1, 2, 3));
-    }
-
-    private void loadGroupKeys() {
-        groupKeys = new Array<>();
-        groupKeys.add(Input.Keys.A);
-        groupKeys.add(Input.Keys.B);
-        groupKeys.add(Input.Keys.C);
     }
 
     public ControlData getNthControl(int id) {
