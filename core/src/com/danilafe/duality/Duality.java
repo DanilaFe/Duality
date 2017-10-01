ad739c7f6ab6465817013ec8eb70ea6eb4ec68a0
Danila Fedorin <danila.fedorin@gmail.com>
2017-10-01 15:50:50 -0700

    Implement the new Duality class.

--- core/src/com/danilafe/duality/Duality.java
+++ core/src/com/danilafe/duality/Duality.java
@@ -1,41 +1,90 @@
 package com.danilafe.duality;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Queue;
+import com.danilafe.duality.controls.ControlManager;
+import com.danilafe.duality.ecs.components.graphics.Camera;
+import com.danilafe.duality.ecs.recipe.RecipeDatabase;
+import com.danilafe.duality.ecs.systems.*;
+import com.danilafe.duality.level.Level;
 
 public class Duality extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    private static Duality instance;
+
+    Queue<DualityRunnable> runQueue;
+    PooledEngine engine;
+
+    ResourceManager resourceManager;
+    RecipeDatabase recipeDatabase;
+	ControlManager controlManager;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		runQueue = new Queue<>();
+		engine = new PooledEngine();
+		resourceManager = new ResourceManager();
+		recipeDatabase = new RecipeDatabase();
+		controlManager = new ControlManager();
+
+		engine.addSystem(new ActiveGroupSystem());
+		EmitterSystem emitterSystem = new EmitterSystem();
+		emitterSystem.resources = resourceManager;
+		engine.addSystem(emitterSystem);
+		engine.addSystem(new ExpiringSystem());
+		engine.addSystem(new AccelerationSystem());
+		engine.addSystem(new FrictionSystem());
+		engine.addSystem(new PlatformSystem());
+		engine.addSystem(new PushSystem());
+		engine.addSystem(new CollisionSystem());
+		engine.addSystem(new PositionSystem());
+		engine.addSystem(new AverageSystem());
+		engine.addSystem(new CameraSystem());
+		engine.addSystem(new CollisionBoxSystem());
+		engine.addSystem(new FollowingSystem());
+		engine.addSystem(new OverlapSystem());
+		engine.addSystem(new SurfaceSystem());
+		engine.addSystem(new RunnableSystem());
+		engine.addSystem(new PlayerSystem());
+		engine.addSystem(new LevelSystem());
+		RenderSystem renderSystem = new RenderSystem();
+		renderSystem.shaderProgram = resourceManager.getShader("default");
+		renderSystem.backgroundTexture = resourceManager.getTexture("background");
+		engine.addSystem(renderSystem);
+
+		Level.loadInternal("test_level").create(engine, resourceManager, recipeDatabase, controlManager);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		engine.update(Gdx.graphics.getDeltaTime());
+		while(runQueue.size != 0) {
+			runQueue.removeFirst().run(engine, resourceManager, recipeDatabase, controlManager);
+		}
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 
-    private static Duality instance;
+	@Override
+	public void resize(int width, int height) {
+		ImmutableArray<Entity> entities = engine.getEntitiesFor(Family.all(Camera.class).get());
+		entities.forEach(e -> e.getComponent(Camera.class).camera.setToOrtho(false, Constants.VIEW_WIDTH, Constants.VIEW_WIDTH* height / width));
+	}
 
-    Queue<DualityRunnable> runQueue;
-
-    public static void runAfterStep(DualityRunnable runnable){
+	public static void runAfterStep(DualityRunnable runnable){
         instance.runQueue.addLast(runnable);
     }
+
 }
